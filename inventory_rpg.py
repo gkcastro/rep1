44aa7cb587b33b72853cfe1ae96909ab0e11b011
gkcastro <gkhayn@gmail.com>
2020-09-02 20:07:23 -0500

    adicionado item no inventário rpg e novo arquivo

--- inventory_rpg.py
+++ inventory_rpg.py
@@ -1,6 +1,6 @@
 # inventory.py
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby', 'iron sword']
 leftwid = 14
 rightwid = 5
 
